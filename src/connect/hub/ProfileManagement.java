1a9feaa5fff7398377fc8bcae6a54c548ef9fca7
SaraRazen <sara3sec@gmail.com>
2024-12-04 15:10:39 +0200

    version 1 task2

---
+++ src/connect/hub/ProfileManagement.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connect.hub;
+
+/**
+ *
+ * @author sarar
+ */
+import java.io.*;
+import java.util.*;
+import org.json.*;
+public class ProfileManagement {
+    //private final UserDataBase userDataBase;
+
+    public ProfileManagement(String userDataBase) {
+        //this.userDataBase=userDataBase;
+    }
+    
+    
+}
+//we have to upload 
