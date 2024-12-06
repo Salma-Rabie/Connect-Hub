7614d6a9f450939336817c28326f758873d82469
Salma-Rabie <sresalma@gmail.com>
2024-12-06 16:03:40 +0200

    Fixed errors in profile edit

--- src/Backend/UserManager.java
+++ src/Backend/UserManager.java
@@ -1,12 +1,11 @@
-
 package Backend;
 
 import static Backend.PasswordHashing.hashPassword;
 import java.time.LocalDate;
 
-
 public class UserManager {
-    private static int idCounter=1000;
+
+    private static int idCounter;
     private final UserDataBase database;
 
     public UserDataBase getDatabase() {
@@ -15,35 +14,43 @@ public class UserManager {
 
     public UserManager(UserDataBase database) {
         this.database = database;
+        idCounter = 1000;
     }
-   
-    public User sigup(String email, String username, String password, LocalDate dateOfBirth){
-        
-        
-            return new User.UserBuilder().dateOfBirth(dateOfBirth)
-                    .email(email).passwordHash(password)
-                    .status("online").username(username).userId(username)
-                    .userId(String.valueOf(idCounter++)).build();
-            
-        
+
+    public User signup(String email, String username, String password, LocalDate dateOfBirth) {
+
+        User user = new User.UserBuilder().dateOfBirth(dateOfBirth)
+                .email(email).passwordHash(password)
+                .status("online").username(username).userId(username)
+                .userId(String.valueOf(idCounter)).build();
+        idCounter++;
+        System.out.println(idCounter);
+        database.saveUser(user);
+        return user;
+
     }
-    public User login(String userId, String password){
-        User user= database.getUserById(userId);
-        if(user==null){
+
+    public User login(String userId, String password) {
+        User user = database.getUserById(userId);
+        if (user == null) {
             return null;
+        } else if (!user.getPasswordHash().equals(hashPassword(password))) {
+            return null;
+        } else {
+            return user;
         }
-            else if(!user.getPasswordHash().equals(hashPassword(password))){
-                  return null;  
-                    }
-            else 
-                return user;
-        
+
     }
-    
-   public  boolean isValidEmail(String email) {
-        //for email validation
-        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$";
+
+    public void logout(String userId, String status) {
+        User user = database.getUserById(userId);
+        user.setStatus(status);
+
+    }
+
+    public boolean isValidEmail(String email) {
+        // For email validation
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
         return email.matches(emailRegex);
     }
-  
 }
